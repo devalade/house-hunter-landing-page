71e6be1efa1c08503d562a0591f89550cac1aec1
devalade <aladecharaf23@gmail.com>
2023-05-15 09:31:29 +0100

    color blur

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,18 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
+        lg: '4rem',
+        xl: '5rem',
+        '2xl': '6rem',
       },
+    },
+    extend: {
+      'content': ['Lexend']
     },
   },
   plugins: [],
