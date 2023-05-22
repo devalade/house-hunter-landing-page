b79a3fb12916cae3339347ee5ec155e4b00e909d
devalade <aladecharaf23@gmail.com>
2023-05-22 08:51:06 +0100

    feat: add the blur effect to eady to sell section

--- src/components/ready-to-sell.tsx
+++ src/components/ready-to-sell.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from 'react';
 import SectionTitle from './section-title';
 import {BathRoomIcon, BedroomIcon, CarportIcon, FloorIcon} from '@/icon';
 import Image from 'next/image';
-import {Button} from './ui';
+import {Button} from '@/components/ui';
 import {PhoneIcon} from '@/icon/phone-icon';
 
 const HOUSE_DETAIL_DATA: (HouseDetailItemProps & { id: number } )[] = [
@@ -30,12 +30,14 @@ const HOUSE_DETAIL_DATA: (HouseDetailItemProps & { id: number } )[] = [
 
 export default function ReadyToSell() {
     return (
-        <section className='mt-[120px] container'>
+        <section className='relative flex items-center justify-between mt-[120px] container'>
+            <span className='absolute -right-28  top-6 bg-[#FF9882] w-60 aspect-square blur-[120px] -z-50 '></span>
+            <span className='absolute right-32  -top-24 bg-[#FFFA82] w-60 aspect-square blur-[120px] -z-50'></span>
             <div className=''>
                 <SectionTitle title='Ready To Sell' subTitle="Let's Tour And See Our House!" />
                 <div className='ml-8 '>
                     <p className=' w-[413px] font-normal text-base text-brandBlue-100'>Houses recommended by our partners that have been curated to become the home of your dreams!</p>
-                    <div className='mt-4 space-y-4'>
+                    <div className='mt-4 space-y-8'>
                         <h6 className='text-base font-semibold text-brandBlue-700'>House Detail</h6>
                         <div className='grid grid-cols-2 w-[524px] gap-3'>
                             { HOUSE_DETAIL_DATA.map( houseDetail => ( <HouseDetailItem key={houseDetail.id} {...houseDetail} /> ))}
@@ -59,8 +61,15 @@ export default function ReadyToSell() {
                     </div>
                 </div>
             </div>
-            <div>
-
+            <div className=' relative flex w-[488px] h-[416px] rounded-sm'>
+                <Image src="/ready_to_sell_image_1.png"  className='absolute inset-0 object-cover w-full h-full'  width={500} height={500} alt='Video cover image' />
+                <div className='absolute bottom-0 flex items-end translate-y-1/3 -translate-x-9 gap-x-3 '>
+                    <Image src="/ready_to_sell_image_2.png" width={296} height={168} alt='Other detail image' />
+                    <div className='flex gap-x-2'>
+                        <Image src="/ready_to_sell_image_3.png" width={96} height={80} alt='Other detail image' />
+                        <Image src="/ready_to_sell_image_3.png" width={96} height={80} alt='Other detail image' />
+                    </div>
+                </div>
             </div>
         </section>
     );
